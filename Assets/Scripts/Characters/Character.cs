44c090dc237711e1bf35fd864de655fda270403a
Trysac <jmgomezsac@gmail.com>
2022-01-20 18:49:17 -0500

    Character Prefebs

--- Assets/Scripts/Characters/Character.cs
+++ Assets/Scripts/Characters/Character.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class Character : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public enum Team
     {
-        
+        Player,
+        Enemy
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [Header("Stats")]
+    [SerializeField] Team team;
+    [SerializeField] string displayName;
+    [SerializeField] int currentHP;
+    [SerializeField] int maxHP;
+
+    [Header("Componentes")]
+    [SerializeField] CharacterEffects characterEffects;
+    [SerializeField] CharacterUI characterUI;
+    [SerializeField] GameObject selectionVisual;
+    [SerializeField] DamageFlash damageFlash;
+
+    [Header("Prefabs")]
+    [SerializeField] GameObject healParticlePrefab;
+
+    private Vector3 standingPosition;
 }
